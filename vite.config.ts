e853c860a152bf32a183aa697d6b2ec8684fb335
stray-code <abc1007020@gmail.com>
2024-04-05 00:14:42 +0900

    manifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import { crx, defineManifest } from '@crxjs/vite-plugin'
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "トップへ戻る",
+  name: "トップへ戻るボタン",
   version: "1.0.0",
+  icons: {
+    16: 'img/icon16.png',
+    48: 'img/icon48.png',
+    128: 'img/icon128.png'
+  },
   content_scripts: [
     {
       js: ["src/content/main.ts"],
