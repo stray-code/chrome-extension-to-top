33baed98af1d94df0da6460cd2d2ec1c5f5610d9
stray-code <abc1007020@gmail.com>
2024-04-10 22:25:46 +0900

    manifestにdescriptionを追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { crx, defineManifest } from '@crxjs/vite-plugin'
 const manifest = defineManifest({
   manifest_version: 3,
   name: "トップへ戻るボタン",
+  description: "トップへ戻るボタンを表示します。",
   version: "1.0.0",
   icons: {
     16: 'img/icon16.png',
