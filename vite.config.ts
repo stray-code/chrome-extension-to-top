24009da40424cf1b8baf9c53527cc43987bd23e7
stray-code <abc1007020@gmail.com>
2024-04-10 22:34:23 +0900

    画像を表示できるようにmanifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { crx, defineManifest } from '@crxjs/vite-plugin'
+import { defineConfig } from "vite";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
@@ -7,18 +7,24 @@ const manifest = defineManifest({
   description: "トップへ戻るボタンを表示します。",
   version: "1.0.0",
   icons: {
-    16: 'img/icon16.png',
-    48: 'img/icon48.png',
-    128: 'img/icon128.png'
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
   },
   content_scripts: [
     {
       js: ["src/content/main.ts"],
       matches: ["http://*/*", "https://*/*"],
-    }
-  ]
-})
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: ["img/*"],
+      matches: ["<all_urls>"],
+    },
+  ],
+});
 
 export default defineConfig({
   plugins: [crx({ manifest })],
-})
+});
