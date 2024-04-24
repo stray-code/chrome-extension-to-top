996ee4197b2cba1938e24b1e95688c6aeceb1582
stray-code <abc1007020@gmail.com>
2024-04-24 23:46:39 +0900

    バージョン更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "トップへ戻るボタン",
   description: "トップへ戻るボタンを表示します。",
-  version: "1.0.1",
+  version: "1.0.2",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",
